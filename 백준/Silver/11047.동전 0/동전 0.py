86f8526931882b4b364d584ce1273f6f7820e826
GY KIM <59333136+crakel@users.noreply.github.com>
2022-02-20 01:09:52 +0900

    [Silver III] Title: 동전 0, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/11047.동전 0/동전 0.py
+++ 백준/Silver/11047.동전 0/동전 0.py
@@ -10,10 +10,10 @@ for _ in range(N):
 
 i = len(lst) - 1
 while i > -1:
-    if sum + lst[i] <= K:
-        sum += lst[i]
-        count += 1
-        i += 1
+    quot = K // lst[i]
+    if quot > 0:
+        sum += quot * lst[i]
+        count += quot
+        K -= quot * lst[i]
     i -= 1
-
 print(count)
