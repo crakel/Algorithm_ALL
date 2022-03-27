579430015db34bb3f513bce691bc3e786a2ce3d4
GY KIM <59333136+crakel@users.noreply.github.com>
2022-03-27 18:25:01 +0900

    [Silver III] Title: N과 M (2), Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,10 @@
+import sys
+from itertools import combinations
+n, m = map(int, sys.stdin.readline().split())
+
+lst = [i for i in range(1, n+1)]
+
+for i in combinations(lst, m):
+    for j in i:
+        print(j, end=' ')
+    print('')
