9bd77e274c30f3178af4f5c63f13ad138ecfd485
GY KIM <59333136+crakel@users.noreply.github.com>
2022-05-05 17:53:29 +0900

    [Silver V] Title: 수들의 합, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -2,16 +2,31 @@ import sys
 
 s = int(sys.stdin.readline())
 
-n_sum = [1]
+# n_sum = [1]
+#
+# i = 1
+# while True:
+#     #print(n_sum[i-1] + i + 1)
+#     if n_sum[i-1] + i + 1 > s:
+#         break
+#
+#     n_sum.append(n_sum[i-1] + i + 1)
+#     i += 1
+#
+# #print(n_sum)
+# print(len(n_sum))
 
-i = 1
-while True:
-    #print(n_sum[i-1] + i + 1)
-    if n_sum[i-1] + i + 1 > s:
-        break
+# 이분탐색 풀이
+ans = 0
+left = 1
+right = s
 
-    n_sum.append(n_sum[i-1] + i + 1)
-    i += 1
+while left <= right:
+    mid = (left + right) // 2
+    if mid * (mid + 1) // 2 <= s:
+        ans = mid
+        left = mid + 1
+    else:
+        right = mid - 1
 
-#print(n_sum)
-print(len(n_sum))
+print(ans)
