25fc8f485af939f14ff8547832a9b657221b6244
GY KIM <59333136+crakel@users.noreply.github.com>
2022-05-26 22:42:16 +0900

    [Silver III] Title: N과 M (6), Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -4,7 +4,21 @@ import itertools
 n, m = map(int, sys.stdin.readline().split())
 
 lst = sorted(list(map(int, sys.stdin.readline().split())))
+# 백트래킹 풀이
+res = []
+def bt(idx):
+    if len(res) == m:
+        print(*res)
+        return
+
+    for i in range(idx, n):
+        if lst[i] not in res:
+            res.append(lst[i])
+            bt(i+1)
+            res.pop()
+
+bt(0)
 
 # 조합 풀이
-for k in itertools.combinations(lst, m):
-    print(*k)
+# for k in itertools.combinations(lst, m):
+#     print(*k)
