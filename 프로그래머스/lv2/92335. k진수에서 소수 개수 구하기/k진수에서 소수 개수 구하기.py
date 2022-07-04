371acf6823fa5334c7429208c41547ed6c8b5d51
GY KIM <59333136+crakel@users.noreply.github.com>
2022-07-04 17:10:44 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -0,0 +1,31 @@
+import math
+
+def solution(n, k):
+    answer = 0
+    
+    def convert(n, k):
+        res = ""
+        if n == 0:
+            return "0"
+        
+        while n > 0:
+            n, mod = divmod(n, k)
+            res += str(mod)
+        return res[::-1]
+    
+    def is_prime(n):
+        if n < 2:
+            return False
+        else:
+            for i in range(2, int(math.sqrt(n)) + 1):
+                if n % i == 0:
+                    return False
+        return True
+
+    #print(convert(n,k))
+    lst = list(convert(n,k).split('0'))
+    
+    for n in lst:
+        if n and is_prime(int(n)):
+            answer += 1
+    return answer
